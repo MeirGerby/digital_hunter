6eaf0478521671624e6fe8f273452fed31ee8e8b
MEIRG <meirg2001@gmail.com>
2026-03-16 11:11:24 +0200

    feat: add the list of the topics SIGNAL_TYPES_TOPICS

--- handle_data/main.py
+++ handle_data/main.py
@@ -2,8 +2,8 @@ from shared import log_event, ConsumerMessage
 
 LOG_LEVEL="DEBUG"
 KAFKA_GROUP_ID="data"
-
+SIGNAL_TYPES_TOPICS: list[str] = ["SIGINT", "VISINT", "HUMINT"]
 class Manager:
     def __init__(self):
-        self.consumer = ConsumerMessage(KAFKA_GROUP_ID)
+        self.consumer = ConsumerMessage(KAFKA_GROUP_ID) 
     
