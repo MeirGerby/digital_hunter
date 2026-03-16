4425839f66afe1db5331257f1ab40257e31d8546
MEIRG <meirg2001@gmail.com>
2026-03-16 16:06:49 +0200

    feat: add model_validator to validate the data while they are not in the target_bank

--- shared/schemas/intel_schema.py
+++ shared/schemas/intel_schema.py
@@ -1,6 +1,8 @@
-from typing import Literal, Annotated
-from pydantic import BaseModel, Field  
+from typing import Literal, Annotated, Any
+from typing_extensions import Self
+from pydantic import BaseModel, model_validator
 from datetime import datetime
+from shared.models.target_bank import TARGET_BANK
 
 
 class IntelSchema(BaseModel):
@@ -10,5 +12,20 @@ class IntelSchema(BaseModel):
     reported_lat: float
     reported_lon: float
     signal_type: Literal["SIGINT", "VISINT", "HUMINT"]
-    priority_level: Annotated[int, Literal[1,2,3,4,5,99]]
+    priority_level: Annotated[int, Literal[1,2,3,4,5,99]]  
+
+    @model_validator(mode='after')
+    def check_target_bank(self) -> Self:
+        """validate if """
+        for i in TARGET_BANK:
+            if i["entity_id"] == self.entity_id:
+                return self
+        self.priority_level = 99 
+        return self 
+    
+
+
+
+    
+
 
