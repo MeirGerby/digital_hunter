b375a06c1ea0ac143e48e88db186008a361479eb
MEIRG <meirg2001@gmail.com>
2026-03-16 11:10:04 +0200

    feat: initiolized the Manager class for managing the naddle_data service

--- handle_data/main.py
+++ handle_data/main.py
@@ -1,2 +1,9 @@
 from shared import log_event, ConsumerMessage 
 
+LOG_LEVEL="DEBUG"
+KAFKA_GROUP_ID="data"
+
+class Manager:
+    def __init__(self):
+        self.consumer = ConsumerMessage(KAFKA_GROUP_ID)
+    
