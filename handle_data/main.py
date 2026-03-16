c24f813cedfffe021ff3a8d84bb52d6a94070926
MEIRG <meirg2001@gmail.com>
2026-03-16 11:23:25 +0200

    refactor: change the topics to ['intel', 'attack', damage']

--- handle_data/main.py
+++ handle_data/main.py
@@ -2,7 +2,7 @@ from shared import log_event, ConsumerMessage
 
 LOG_LEVEL="DEBUG"
 KAFKA_GROUP_ID="data"
-SIGNAL_TYPES_TOPICS: list[str] = ["SIGINT", "VISINT", "HUMINT"]
+SIGNAL_TYPES_TOPICS: list[str] = ["intel", "attack", "damage"]
 class Manager:
     def __init__(self):
         self.consumer = ConsumerMessage(KAFKA_GROUP_ID)  
