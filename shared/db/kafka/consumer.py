8dac202bc22823d08703c6089df0320a8f172d9c
MEIRG <meirg2001@gmail.com>
2026-03-16 10:32:05 +0200

    feat: add ConsumerMessage class in shared folder

---
+++ shared/db/kafka/consumer.py
@@ -0,0 +1,38 @@
+import json
+
+from confluent_kafka import Consumer
+
+KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
+KAFKA_TOPIC=""
+class ConsumerMessage:
+    def __init__(self):
+        self.config = {
+            "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
+            "group.id": KAFKA_TOPIC,
+            "auto.offset.reset": "earliest"
+        }
+        self.consumer = Consumer(self.config)
+
+    def consumer_loop(self, callback):
+        self.consumer.subscribe([KAFKA_TOPIC])
+
+        print(f"Consumer is running and subscribed to {KAFKA_TOPIC} topics")
+
+        try:
+            while True:
+                msg = self.consumer.poll(1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    print("Error:", msg.error())
+                    continue
+
+               
+                data = json.loads(msg.value().decode("utf-8")) # type: ignore 
+                callback(data) 
+
+        except KeyboardInterrupt:
+            print(" the consumer stops by the user")
+
+        finally:
+            self.consumer.close()
