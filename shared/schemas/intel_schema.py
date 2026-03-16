1f893f02724a17ffbddd8b4665a21f8a557ef9e0
MEIRG <meirg2001@gmail.com>
2026-03-16 16:36:28 +0200

    feat: add validation with model_validator for colculate the distance (default 0)

--- shared/schemas/intel_schema.py
+++ shared/schemas/intel_schema.py
@@ -2,7 +2,8 @@ from typing import Literal, Annotated
 from typing_extensions import Self
 from pydantic import BaseModel, model_validator, Field 
 from datetime import datetime
-from shared.models.target_bank import TARGET_BANK
+from shared.models import target_bank 
+from shared.utils import haversine_km
 
 
 class IntelSchema(BaseModel):
@@ -13,18 +14,22 @@ class IntelSchema(BaseModel):
     reported_lon: float
     signal_type: Literal["SIGINT", "VISINT", "HUMINT"]
     priority_level: Annotated[int, Literal[1,2,3,4,5,99]] 
-    haversine: Annotated[int, Field(default=0, ge=0)]
+    distance: Annotated[float, Field(default=0, ge=0)]
 
     @model_validator(mode='after')
     def check_target_bank(self) -> Self:
         """validate if """
-        for i in TARGET_BANK:
+        for i in target_bank:
             if i["entity_id"] == self.entity_id:
+                self.distance = haversine_km(i["lat"], i["lon"], self.reported_lat, self.reported_lon)
                 return self
         self.priority_level = 99 
         return self 
     
 
+msg = {"timestamp": "2026-03-16T09:47:06.866256+00:00", "signal_id": "f08996fd-5034-45f6-943f-9c7767dbc280", "entity_id": "TGT-015", "reported_lat": 31.514866, "reported_lon": 34.45254, "signal_type": "VISINT", "priority_level": 1}                
+intel = IntelSchema(**msg)
+print(intel)
 
 
     
