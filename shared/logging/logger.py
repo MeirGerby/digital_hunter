eec5a630c628700dffe9fd585ba78aa89d902bac
MEIRG <meirg2001@gmail.com>
2026-03-16 10:37:22 +0200

    refactor: change the hardcoded url to a veriable ELASTIC_URL for handling it after from the env file

--- shared/logging/logger.py
+++ shared/logging/logger.py
@@ -20,9 +20,10 @@ Note: If Elasticsearch is unreachable, the log will fallback to the console (Loc
 from elasticsearch import Elasticsearch
 from datetime import datetime
 
+ELASTIC_URL='http://localhost:9200'
 # Initialize Elasticsearch client
 # Ensure the host 'localhost' matches your docker-compose configuration
-es = Elasticsearch(['http://localhost:9200'])
+es = Elasticsearch([ELASTIC_URL])
 
 
 def log_event(level, message, extra_info=None):
