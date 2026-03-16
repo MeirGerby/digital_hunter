1090a0928cca3a58fd7c00ac49fe2b331a678a5c
MEIRG <meirg2001@gmail.com>
2026-03-16 15:03:56 +0200

    feat: import the producerMessage from shared library

--- handle_data/main.py
+++ handle_data/main.py
@@ -1,19 +1,29 @@
-from shared import log_event, ConsumerMessage 
+from shared import (
+    log_event, 
+    ConsumerMessage,
+    ProducerMessage,
+    DamageSchema,
+    AttackSchema,
+    IntelSchema
+)
 
+KAFKA_SIGNALS_INTEL_TOPIC = "dlq_signals_intel"
 LOG_LEVEL="DEBUG"
 KAFKA_GROUP_ID="data"
 SIGNAL_TYPES_TOPICS: list[str] = ["intel", "attack", "damage"]
 class Manager:
     def __init__(self):
-        self.consumer = ConsumerMessage(KAFKA_GROUP_ID)  
+        self.consumer = ConsumerMessage(KAFKA_GROUP_ID) 
+        self.producer = ProducerMessage() 
     
-    def data_manager(self):
+    def data_manager(self, data: dict):
         """the callback function for handling and managing the coming data"""
-        pass 
+        pass
+
 
     def main(self):
         """start the program by running the consumer loop"""
-        self.consumer.consumer_loop( self.data_manager, SIGNAL_TYPES_TOPICS) 
+        self.consumer.consumer_loop(self.data_manager, SIGNAL_TYPES_TOPICS) 
 
 if __name__ == "__main__":
     manager = Manager()
