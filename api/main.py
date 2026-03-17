c93ec39d73236315cbfcbad8d7570f3834b63371
MEIRG <meirg2001@gmail.com>
2026-03-17 15:50:21 +0200

    feat: add the running point for the program

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from .routes import router 
+from shared import MySQLConnector 
+
+
+app = FastAPI()
+app.include_router(router) 
+
+if __name__ == "__main__":
+    import uvicorn 
+    uvicorn.run("api.main:app", host="localhost", port=8000, reload=True)
