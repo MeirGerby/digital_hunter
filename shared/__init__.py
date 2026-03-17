5ec93f7638c390b1aac0bde0f1d705183fb82c64
MEIRG <meirg2001@gmail.com>
2026-03-17 16:49:21 +0200

    feat: import the settings from core level

--- shared/__init__.py
+++ shared/__init__.py
@@ -3,3 +3,4 @@ from .logging import log_event
 from .schemas import IntelSchema, DamageSchema, AttackSchema 
 from .models import target_bank
 from .utils import haversine_km 
+from .core import settings 
