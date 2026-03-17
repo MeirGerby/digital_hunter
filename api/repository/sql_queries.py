b33d08ef1e1179261b8b77e158fb873b77388cdb
MEIRG <meirg2001@gmail.com>
2026-03-17 12:13:58 +0200

    feat: add MysqlQueries.analysis_collection_sources method

--- api/repository/sql_queries.py
+++ api/repository/sql_queries.py
@@ -23,6 +23,26 @@ class MysqlQueries:
         conn.close_connection()
         return data 
     
+    def analysis_collection_sources(self, conn: MySQLConnector):
+        cursor = conn.get_connection().cursor()
+        query = """
+                SELECT 
+                    intel_signals.signal_type,
+                    COUNT(intel_signals.signal_type) as count_signles
+                FROM intel_signals
+                GROUP BY 
+                    intel_signals.signal_type 
+                ORDER BY 
+                    COUNT(intel_signals.signal_type) DESC;
+                """
+        cursor.execute(query)
+        data = cursor.fetchall()
+        conn.close_connection()
+        return data 
 
+if __name__ == "__main__":
+    conn = MySQLConnector() 
+    query = MysqlQueries().analysis_collection_sources(conn)
+    print(query)
 
     
